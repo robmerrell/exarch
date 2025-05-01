3b5e15e3e515a3b3c378b71db1d37798a614b264
Rob Merrell <rob@robm.dev>
2025-05-01 04:23:30 +0000

    search test

--- internal/search/search_test.go
+++ internal/search/search_test.go
@@ -1,87 +1,23 @@
 package search
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
 
-func TestSearchWithString(t *testing.T) {
-	input := &SearchInput{
-		SearchType:  SearchTypeStr,
-		SearchTerms: "string",
-	}
-
-	matches, err := searchFile("testdata/users.ex", input)
+func TestSearchFile(t *testing.T) {
+	res, err := searchFile("testdata/users.ex", &SearchInput{
+		SearchTerms: "process",
+		SearchType:  SearchTypeFnCall,
+		Dir:         "",
+	})
 	if err != nil {
-		t.Errorf("searching file failed: %+v", err)
+		t.Errorf("failed searchFile: %v", err)
 	}
 
-	expected := []Match{
-		{Row: 40, Contents: `"string one"`},
-		{Row: 41, Contents: `"string two"`},
-		{Row: 42, Contents: `"string three"`},
-	}
+	expected := []string{"20:TestApp.FilterChain.process()"}
 
-	if !reflect.DeepEqual(matches, expected) {
-		t.Errorf("got %v want %v", matches, expected)
+	if !reflect.DeepEqual(res, expected) {
+		t.Errorf("got %v want %v", res, expected)
 	}
 }
-
-func TestSearchWithAtom(t *testing.T) {
-	input := &SearchInput{
-		SearchType:  SearchTypeAtom,
-		SearchTerms: "ber",
-	}
-
-	matches, err := searchFile("testdata/users.ex", input)
-	if err != nil {
-		t.Errorf("searching file failed: %+v", err)
-	}
-
-	expected := []Match{
-		{Row: 30, Contents: ":blueberry"},
-		{Row: 31, Contents: ":strawberry"},
-	}
-
-	if !reflect.DeepEqual(matches, expected) {
-		t.Errorf("got %v want %v", matches, expected)
-	}
-}
-
-func TestSearchWithAlias(t *testing.T) {
-	input := &SearchInput{
-		SearchType:  SearchTypeAlias,
-		SearchTerms: "",
-	}
-
-	matches, err := searchFile("testdata/users.ex", input)
-	if err != nil {
-		t.Errorf("searching file failed: %+v", err)
-	}
-
-	fmt.Println(matches)
-
-	// expected := []Match{
-	// 	{Row: 30, Contents: ":blueberry"},
-	// 	{Row: 31, Contents: ":strawberry"},
-	// }
-
-	// if !reflect.DeepEqual(matches, expected) {
-	// 	t.Errorf("got %v want %v", matches, expected)
-	// }
-}
-
-// func TestSearchWithStringIgnoresComments(t *testing.T) {
-// 	input := &SearchInput{
-// 		SearchType:  SearchTypeStr,
-// 		SearchTerms: "user",
-// 	}
-
-// 	matches, err := searchFile("testdata/users.ex", input)
-// 	if err != nil {
-// 		t.Errorf("searching file failed: %+v", err)
-// 	}
-
-// 	fmt.Println(matches)
-// }
